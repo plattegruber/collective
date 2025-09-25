4761f3f30c998d45cd36792f107a865872a5c5b2
Platte Gruber <gruberplatte@gmail.com>
2025-09-24 22:53:29 -0500

    Register Workers Subdomain and Update Deploy Flow

--- infra/main.tf
+++ infra/main.tf
@@ -19,9 +19,18 @@ variable "account_id" {
   type = string
 }
 
+variable "workers_subdomain" {
+  type = string
+}
+
 resource "cloudflare_workers_kv_namespace" "reactions" {
   account_id = var.account_id
   title      = "gg-reactions"
+}
+
+resource "cloudflare_workers_subdomain" "default" {
+  account_id = var.account_id
+  subdomain  = var.workers_subdomain
 }
 
 output "kv_namespace_id" {
